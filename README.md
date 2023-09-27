bae5cf06d58a909799763214e1b7c387fd902108
divyyeshchandra <divyyesh.bvp23@gmail.com>
2023-09-27 16:11:28 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+1. Clone and install dependencies
+
+# git clone
+
+# npm i
+
+2. Start the server
+
+# npm run dev
---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import PostFeed from "../components/PostFeed";
+import {
+  getFollowing,
+  getPublications,
+  getPublicationsQueryVariables,
+} from "../constants/lensConstants";
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
+import { useLensContext } from "../context/LensContext";
+import { apolloClient } from "../constants/lensConstants";
+
+let profileIdList = ["0x869c"];
+
+export default function Home(props) {
+  const { profileId } = useLensContext();
+  const { account } = useMoralis();
+  const [pubs, setPubs] = useState();
+
+  const getPublicationsList = async () => {
+    let followers;
+    let followingsIds = [];
+    if (account) {
+      followers = await apolloClient.query({
+        query: getFollowing,
+        variables: { request: { address: account } },
+      });
+      followingsIds = followers.data.following.items.map((f) => f.profile.id);
+    }
+    profileIdList = profileIdList.concat(followingsIds);
+    const publications = await apolloClient.query({
+      query: getPublications,
+      variables: getPublicationsQueryVariables(profileIdList),
+    });
+    return publications;
+  };
+
+  useEffect(() => {
+    if (account) {
+      getPublicationsList().then((publications) => {
+        setPubs(publications);
+      });
+    }
+  }, [account]);
+
+  return (
+    <main className="pt-24">
+      <div className="pl-2 pr-2">
+        <div className="p-2 m1 bg-sky-600 border text-white rounded  border-solid border-black">
+          <p>Our decentralized social media platform!</p>
+        </div>
+      </div>
+
+      {!profileId ? (
+        <div>
+          You aren't following anyone yet or you don't have any profileId!)
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      {!pubs ? (
+        <p>Loading...</p>
+      ) : (
+        <PostFeed posts={pubs.data.publications.items} />
+      )}
+    </main>
+  );
+}
